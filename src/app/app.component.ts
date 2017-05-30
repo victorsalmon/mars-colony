cea7478a4cd761e5b63f2eebd104f5e10e0369ae
Victor Salmon <vasalmon@hotmail.com>
2017-05-30 09:34:13 -0700

    added routing and migrated home component html scss to home from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,4 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  title = 'Mars Colony 1';
-  subtitle = 'Tap circle to enter';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes} from '@angular/router'; // Router
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { EncountersComponent } from './pages/encounters/encounters.component';
 import { ReportComponent } from './pages/report/report.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'encounters', component: EncountersComponent },
+  { path: 'report', component: ReportComponent }
+];
 
 @NgModule({
   declarations: [
@@ -20,7 +28,8 @@ import { ReportComponent } from './pages/report/report.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes) // Router
   ],
   providers: [],
   bootstrap: [AppComponent]
