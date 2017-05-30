30df8b93078a830f41dae417c343e3ba10bd979f
Victor Salmon <vasalmon@hotmail.com>
2017-05-30 15:31:15 -0700

    Formatted alien data output and populated selection box on report component (report an alien)

--- src/app/pages/report/report.component.ts
+++ src/app/pages/report/report.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Alien } from '../../models/alien';
-import { RegisterService } from '../../services/register.service';
-//Report an Encounter
+import { ReportService } from '../../services/report.service';
+// Report an Encounter
 
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
-  styleUrls: ['./report.component.scss']
+  styleUrls: ['./report.component.scss'],
+  providers: [ReportService]
 })
 export class ReportComponent implements OnInit {
-
-  constructor() { }
+  public listAliens: Alien [] = [];
+  constructor(private reportService: ReportService) { }
 
   ngOnInit() {
+    this.reportService.getData()
+        .subscribe((aliens) => {
+          this.listAliens = aliens;
+        });
   }
-
 }
--- src/app/services/report.service.ts
+++ src/app/services/report.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import { Alien } from '../models/alien';
 
 @Injectable()
 export class ReportService {
+  private URL_ALIENS = 'https://red-wdp-api.herokuapp.com/api/mars/aliens';
 
-  constructor() { }
+  constructor(private http: Http) { }
 
+  getData() {
+    return this.http.get(this.URL_ALIENS)
+      .map(this.extractAliens);
+  }
+
+  extractAliens (res: Response) {
+    const aliens = res.json();
+    return aliens.aliens;
+  }
 }
