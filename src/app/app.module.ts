8e9a62d1b2dfcc793be04200f70348e25edc374f
Victor Salmon <vasalmon@hotmail.com>
2017-06-02 15:36:07 -0700

    Added blog component to begin working on REST-API stretch goal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { EncountersComponent } from './pages/encounters/encounters.component';
 import { ReportComponent } from './pages/report/report.component';
+import { BlogComponent } from './pages/blog/blog.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'encounters', component: EncountersComponent },
-  { path: 'report', component: ReportComponent }
+  { path: 'report', component: ReportComponent },
+  { path: 'blog', component: ReportComponent }
 ];
 
 @NgModule({
@@ -23,7 +25,8 @@ const appRoutes: Routes = [
     HomeComponent,
     RegisterComponent,
     EncountersComponent,
-    ReportComponent
+    ReportComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
